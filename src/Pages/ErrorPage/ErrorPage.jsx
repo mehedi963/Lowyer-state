5d14a9f8aaa240eb85a1ad2bc4e51f9a806fef1a
Your NameMehedi <you@example.com>
2025-05-18 07:09:09 -0600

    added changes

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router';
 const ErrorPage = () => {
     return (
         <div className='flex flex-col justify-center items-center'>
-            <img src={image404} alt="" />
+            <img className='w-3/5 mx-auto' src={image404} alt="" />
             <h1 className='text-3xl font-bold text-fuchsia-500 my-2'>404 - Page Not Found</h1>
-            <span>Ooops! The page you're looking for doesn't exist</span>
+            <span className='mb-2'>Ooops! The page you're looking for doesn't exist</span>
             <Link to='/'><button className='btn btn-active btn-secondary'>Go Back</button></Link>
         </div>
     );
